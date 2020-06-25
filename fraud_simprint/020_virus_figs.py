19364d0b016e46d51e1f87cf82f4501a573f89a8
Ted Dunstone <ted@biometix.com>
2020-06-26 07:32:05 +0800

    simprint update

--- fraud_simprint/020_virus_figs.py
+++ fraud_simprint/020_virus_figs.py
@@ -1,36 +1,67 @@
 # Load the support variables
-from px_build_doc.util import TrendsManager, FigureManager
+import re
+import contextlib
 import statistics
+import collections.abc
+import pickle
+import os.path
+
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+
+import yaml
+import jinja2
+
 import pandas as pd
 import seaborn as sns
-import yaml
+
+from px_build_doc.util import TrendsManager, FigureManager
 from px_build_doc.util import FigureManager, TableManager, fetch_var, display
-import jinja2
-import re
 
-
+## Util Functions
 def raise_error(error_str):
-  raise ValueError("\n\n"+error_str)
+  raise ValueError("\n\n"+str(error_str))
+
+def deep_update(d,u):
+    """
+    Update a nested dictionary or similar mapping.
+    """
+    for k, v in u.items():
+        if isinstance(d, collections.abc.Mapping):
+            if isinstance(v, collections.abc.Mapping):
+                r = deep_update(d.get(k, {}), v)
+                d[k] = r
+            else:
+                d[k] = u[k]
+        else:
+            d = {k: u[k]}
+    return d
+
+## Report Functions
 
 class ReportProcess():
 
   global_data_dict = {} # the data dictionary - holds all ddefined data variables
-
-
+  observation = {}
+  
   def __init__(self,filename):
-    
+    sns.set()
     with open(filename) as f:
       sns.set(style="ticks", palette="pastel")
 
-      self.bp=self.load_yaml(f.read())
-
-      self.tables = TableManager(format=self.meta.output_type) 
-      self.figs = FigureManager(format=self.meta.output_type)
+      self.bps=self.load_yaml(f.read())
+      self.tables = TableManager(format=self.doc_format) 
+      self.figs = FigureManager(format=self.doc_format)
 
       try:
+        # build the reports
+        self.bp = self.bps[0]
         self.load_data(self.bp)
-        self.build_report(self.bp['report'])
-#        self.run_analysis(bp)
+        if len(self.bps)>0:
+          for bp in self.bps[1:]:
+            self.create_md_doc(bp)
+ 
       except Exception: #or AssertionError
         import traceback
         traceback.print_exc(limit=10)
@@ -39,17 +70,26 @@ class ReportProcess():
   def load_yaml(self,yaml_str):
     """ Load the blue print, if variables are defined substitute them
     Note that the variables must be defined as an array """
-    variables_str=""
-    for line in yaml_str.splitlines():
-      if line.startswith('variables'):
-          variables_str+=line+"\n"
-      elif len(variables_str)>0:
-          variables_str+=line+"\n"
-          if len(line.strip())==0:
-            break
-    blueprint = yaml.load(variables_str,Loader=yaml.Loader)
-    # allow varibale definitiions
-    if 'variables' in blueprint:
+
+    def get_variables_dict(yaml_str):
+      """ get the defined variables """
+
+      # Can't read enitre yaml as jinja2 may cause invalid yaml
+      variables_str=""
+      for line in yaml_str.splitlines():
+        if line.startswith('variables'):
+            variables_str+=line+"\n"
+        elif len(variables_str)>0:
+            variables_str+=line+"\n"
+            if len(line.strip())==0:
+              break
+      if len(variables_str)==0:
+        # no variables defined
+        return {}
+
+      blueprint = yaml.load(variables_str,Loader=yaml.Loader)
+
+      if 'variables' in blueprint:
         # resubstitute vairables 
         var_dict={}
         for var_list in blueprint['variables']:
@@ -61,69 +101,93 @@ class ReportProcess():
             #    val=var_dict[list(val)[0]]
                 
             var_dict[key]=val
-        
-        template = jinja2.Template(yaml_str)
-        yaml_str = template.render(**var_dict)
+      return var_dict
 
-        blueprint = yaml.load(yaml_str,Loader=yaml.Loader)
 
-    self.meta = self.to_obj(blueprint,key='meta',defaults={'output_type':'md'})
-    return blueprint
+
+    def update_yaml(yaml_str,var_dict={}):      
+      template = jinja2.Template(yaml_str)
+      yaml_str = template.render(**var_dict)
+      #open('temp.yaml','w').write(yaml_str)
+      blueprints = yaml.load_all(yaml_str,Loader=yaml.Loader)
+      return list(blueprints)
+
+    var_dict = get_variables_dict(yaml_str)
+    
+    blueprints = update_yaml(yaml_str,var_dict)
+
+    #  call import yamls
+    imported = {}
+    setup_bp = blueprints[0]
+    if 'import' in setup_bp:
+      for filename in setup_bp['import']:
+          with open(filename+'.yaml') as f:
+            imported = list(yaml.load_all(f.read(),Loader=yaml.Loader))[0]
+            setup_bp = deep_update(imported,setup_bp)
+
+    #raise_error(list(setup_bp['analysis']))
+    
+    self.doc_meta = self.get_doc_meta(setup_bp,defaults={'output_type':'md','test':'test'})
+    self.doc_format = self.doc_meta['output_type']
+    
+    blueprints[0] = setup_bp
+    
+    return blueprints
+
+  def get_doc_meta(self,blueprint,defaults):
+    """ Get the document meta data where available"""
+
+    newdict=defaults.copy()
+    if 'doc_meta' in blueprint:
+      newdict = deep_update(newdict,blueprint['doc_meta'])
+    return newdict
+
 
   # turn dict into object
   def to_obj(self,indict,key=False,defaults={}):
-    #print(indict)
     if key:
       indict = indict[key] if key is indict else {}
-    defaults.update(indict)
-    #print(defaults)
-    return type('from_dict', (object,), defaults)
+    return type('from_dict', (object,), deep_update(defaults,indict))
+
+  def do_query(self,query_str):
+    return pd.read_sql_query(query_str, self.conn)
 
   def load_data(self,bp):
-    """ Load all the data from various sources """
+    """ 
+    Load all the data from various sources 
+    cache results in "global_data.sav" """
 
     import sqlite3
     
-    conn = sqlite3.connect("pxdata")
+    self.conn = sqlite3.connect("pxdata")
+
+    cache_data_file = 'global_data.sav'
+    if os.path.exists(cache_data_file):
+      with open(cache_data_file,'rb') as f:
+        self.global_data_dict=pickle.load(f)
 
     # build query dicitionary
-    for result in bp['data']:
-      for key,val in result.items():
-          data_query = self.to_obj(val,defaults={'enabled':True,"query":False,'fields':False,'table':False})
-          if data_query.enabled:
-            if data_query.fields:
-              data_query.table = " from "+data_query.table if data_query.table else ''
-              data_query.query = "select "+data_query.fields + data_query.table +" "+ data_query.query
-            df = pd.read_sql_query(data_query.query, conn) if data_query.query else None
-            self.global_data_dict[key]=df
+    #raise_error(list(bp['data'].keys()))
+    for key,val in bp['data'].items():
+        data_query = self.to_obj(val,defaults={'enabled':True,"query":"",'fields':False,'table':False})
+        if data_query.enabled:
+          if data_query.fields:
+            data_query.table = " from "+data_query.table if data_query.table else ''
+            data_query.query = "select "+data_query.fields + data_query.table +" "+ data_query.query
+          # if cached and the query is the same then just use cache
+          if key in self.global_data_dict and self.global_data_dict['_q_'+key]==data_query.query:
+            continue
 
-  def run_analysis(self,bp):
-    """ Run analysis on the loaded data """
-    tables = self.tables
-    figs = self.figs
-    
-    for result in bp['analysis']:
-      for key,val in result.items():
-          #pylint: disable=no-member
-          analysis = self.to_obj(val,defaults={'enabled':False,"use_data":False,'table':False,"before":'',"after":''})
-          if analysis.enabled:
-            if analysis.use_data:
-                df=self.global_data_dict[analysis.use_data]
+          df = self.do_query(data_query.query) if data_query.query else None
 
-          display("\n"+analysis.before+"\n")
-          if analysis.plot: 
-            ccmpiled = compile(analysis.plot,'py_vars', 'exec')
-            exec(ccmpiled,globals(),locals())
-          if analysis.table:
-            tables.read_df(globals()[analysis.table['variable']]).display(analysis.table['caption']) #pylint: disable=unsubscriptable-object
-          
-          display("\n"+analysis.after+"\n")
+          self.global_data_dict[key]=df
+          self.global_data_dict['_q_'+key]=data_query.query
+
+    with open(cache_data_file,'wb') as f:
+      pickle.dump(self.global_data_dict,f)
+
 
   def format_caption(self,obj):
-
-    #if self.local_caption: 
-    #  return self.local_caption
-    #else:
     return obj.caption
 
 
@@ -140,9 +204,9 @@ class ReportProcess():
     ```
     """
     import matplotlib.pyplot as plt
-    #raise_error(str(analysis.plot))
     if analysis.plot:
-      plot_obj=self.to_obj(analysis.plot,defaults={"labels":False,"caption":False,"height":8})
+      plot_obj=self.to_obj(analysis.plot,defaults={}) #defaults={"labels":False,"caption":False,"height":5,"format":self.doc_format})
+      #raise_error(str(analysis.plot)+str(plot_obj.height))
 
       if plot_obj.labels:
           labs=[l.strip() for l in plot_obj.labels.split(',')] #pylint: disable=no-member
@@ -151,7 +215,7 @@ class ReportProcess():
           if len(labs)>1:
             plt.ylabel(labs[1])
       if plot_obj.caption:
-        figs.save_plot(self.format_caption(plot_obj),height=plot_obj.height).display()
+        figs.save_plot(self.format_caption(plot_obj),height=plot_obj.height,format=plot_obj.format).display()
   
   def handle_table(self,tables,analysis):
     """ Create a table when specified in the analysis
@@ -168,9 +232,9 @@ class ReportProcess():
     """
     
     if analysis.table:
-      table_obj=self.to_obj(analysis.table,defaults={"variable":False,"caption":"","height":8})
+      table_obj=self.to_obj(analysis.table,defaults={})
       if table_obj.variable:
-        tables.read_df(globals()[table_obj.variable]).display(self.format_caption(table_obj))
+        tables.read_df(globals()[table_obj.variable]).display(self.format_caption(table_obj),floatfmt=table_obj.floatfmt)
 
   def handle_uml(self,figs,analysis):
     """ Create a uml diagram using plantuml
@@ -185,16 +249,23 @@ class ReportProcess():
     ```
     """
     if analysis.plot and analysis.uml:
-      uml_obj=self.to_obj(analysis.plot,defaults={"variable":False,"caption":"","height":8})
+      uml_obj=self.to_obj(analysis.plot,defaults={}) #"variable":False,"caption":"","height":8})
       if analysis.plot:
-        figs.set_uml("wbs",self.format_caption(uml_obj),height=uml_obj.height,uml=analysis.uml).display()
+        figs.set_uml("uml",self.format_caption(uml_obj),height=uml_obj.height,uml=analysis.uml).display()
+  
 
   def analysis_func(self,analysis_name,func_params=None):
     """ Process the analysis function name """
 
-    tables = self.tables # define vars avaliable to any anlysis function
+    tables = self.tables # define vars avaliable to any analysis function
     figs = self.figs
-    self.local_caption = None
+
+    def get_data(source_name):
+      """ return the data from a query"""
+
+      if not source_name in self.global_data_dict:
+        raise_error("Error data source %s is not definied."%analysis.use_data)
+      return self.global_data_dict[source_name]
 
     def split_params(name):
       """ split function and parameters
@@ -218,51 +289,36 @@ class ReportProcess():
           use_data=use_data[1:] # strip first char as its a :
       return name,func_params, use_data
 
-    def str_to_dict(instr):
-      """ take a string with named parameters and convert it to a python dictionary """
-      if len(instr.strip())==0:
-        return {}
-      param_split = lambda instr,fld_no: [s.split('=')[fld_no].strip() for s in instr.split(',')]
-      dict_str='{%s}'%','.join([f"'{key.replace('.','_')}':{val}" for key,val in zip(param_split(instr,0),param_split(instr,1))])
-      return eval(dict_str)
     
-    def set_func_params(func_name,func_params_str):
-      """ build a dict for the analysis variablies handel both one line str and dict form"""
-      param_split = lambda instr: [s.strip() for s in instr.split(',')]
-
-      func_params_def_vars = param_split(func_params_str)
-         
-      func_var_dict = str_to_dict(func_params_str)
-      if analysis.params:
-        required_params = param_split(analysis.params)
-        params_in=[a for a in func_params_def_vars if not a.startswith('_')]
-        #if len(required_params)!=len(params_in):
-        #  raise_error("Number of parameters (%s,%s) wrong for function [%s]"%(str(required_params),str(params_in),func_name))
-
-      options={}
-      for key,val in func_var_dict.items():
-        if key.startswith('_'):
-          var_names = key.split('_')
-          if len(var_names)==3:
-            if var_names[1] not in options:
-                options[var_names[1]]={}
-            options[var_names[1]].update({var_names[2]:val})
-      #if len(options)>0:
-      #  self.rasie_error(str(options))
-
-      if '_plot_caption' in func_var_dict:
-            self.local_caption=func_var_dict['_plot_caption']
-
-      return func_var_dict
-
-    def run_function(var_params_dict,analysis,analysis_name,df):
+    def run_function(var_params_dict,analysis_code,analysis_name,df):
       """ run the analysis function in the current context"""
 
       # note df provdied as local to analysis function
+      tables = self.tables
+      figs = self.figs
+      get_df = get_data
+      def set_observation(key,info):
+        if not key in self.observation:
+          self.observation[key]=[]
+        self.observation[key].append(info)
+
+      observations = self.observation
+
 
       _local = locals().copy()
-      _local.update(var_params_dict)
-      ccmpiled = compile(analysis.py,'py_vars', 'exec')
+      _local = deep_update(_local,var_params_dict)
+    
+      if type(analysis.params)==type([]):
+        for param in analysis.params:
+          if param not in _local:
+            p_var=param.split('=')
+            if len(p_var)==1:
+              _local[param] = None
+            else:
+              _local[p_var[0]] = p_var[1]
+      
+      #_local.update(var_params_dict)
+      ccmpiled = compile(analysis_code,'py_vars', 'exec')
       try:
         exec(ccmpiled,globals(),_local)
       except:
@@ -270,11 +326,15 @@ class ReportProcess():
 
 
     analysis_name,func_params_str,use_data = split_params(analysis_name)
-    defaults={'enabled':False,"use_data":False,'table':False,"before":'',"after":'',"uml":False,"py":False,"params":False}
+    defaults={'enabled':False,"use_data":False,'table':False,"before":'',"after":'',
+                "uml":False,"py":False,"after_py":False,"params":False,
+                "plot":{"labels":False,"caption":False,"height":7,"format":self.doc_format},
+                "table":{"variable":False,"caption":"","height":8,"floatfmt":()}}
     
     analysis_dict = self.bp['analysis'][analysis_name]
     if func_params is not None:
-      analysis_dict.update(func_params)
+      #analysis_dict.update(func_params)
+      analysis_dict = deep_update(analysis_dict,func_params)
     else:
       func_params={}
    
@@ -284,30 +344,63 @@ class ReportProcess():
         analysis.use_data = use_data
 
     if analysis.use_data:
-        if analysis.use_data in self.global_data_dict:
-          df=self.global_data_dict[analysis.use_data]
-        else:
-          raise_error("Error data source %s is not definied.")
-
-    #func_var_dict = set_func_params(analysis_name,func_params_str)
-    #func_var_dict.update(func_params)
-
+        df=get_data(analysis.use_data)
+    else:
+        df=None
+    
     display("\n"+analysis.before+"\n")
     
     if analysis.py:
-      run_function(func_params,analysis,analysis_name,df)
-      #raise_error(analysis_name+':'+str(func_params)+str(analysis.plot))
+      run_function(func_params,analysis.py,analysis_name,df)
+      #raise_error(str(analysis.plot))
       self.handle_plot(figs,analysis)
       self.handle_table(tables,analysis)
+
+    if analysis.after_py:
+      run_function(func_params,analysis.after_py,analysis_name,df)
+      #raise_error(str(analysis.plot))
+       
   
     self.handle_uml(figs,analysis)    
     
     display("\n"+analysis.after+"\n")
     
+  def create_md_doc(self, blueprint):
+    """ Build the doc from a blueprint dict """
 
-  def build_report(self, item_list, level=1):
-    for result in item_list:
+    # allow definition of local doc params
+    doc_meta = self.get_doc_meta(blueprint,defaults=self.doc_meta)
+    for doc_name,doc_def in blueprint.items():
+      if not doc_name == 'doc_meta': 
+        with open(doc_name+'.md','w') as f:
+         #with contextlib.redirect_stdout(f):
+            print("---")
+            print(yaml.dump(doc_meta))
+            print("---")
+            self.build_md_doc(doc_def)
+
+    # save config yaml
+    #with open(doc_name+'.yaml', 'w') as file:
+    #    yaml.dump(doc_meta, file)
+
+
+  def build_md_doc(self, item_list, level=1):
+    """ Recursively builds a markdown document from a dict
+    
+    Rules are:
+    * If value is a string treat as markdown
+    * If a key:
+     * contains a () then it is an analysis, params are in the dict
+     * **file**: load markdown file
+    """
+    #raise_error(str(item_list))
+    for item_num, result in enumerate(item_list):
       for key,val in result.items():
+        
+      #  if key =='doc_meta': # allow definition of local doc params
+      #      self.doc_meta.update(val)
+      #      continue
+
         display('#'*level+f" {key}")
         for cmds in val:
           if type(cmds)==type(""):
@@ -326,10 +419,10 @@ class ReportProcess():
               elif key1=='file':
                   with open(val1) as f:
                     display("\n"+f.read()+'\n')
+              
               if type(val1) == type([]):
-                # recursive call
-                #display('#'*(level+1)+f" {key1}")
-                self.build_report([cmds],level+1)
+                # recursive call for lower heading levels
+                self.build_md_doc([cmds],level+1)
 
 
 ReportProcess('analysis.yaml')
